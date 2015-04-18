2f4d1459bf92e44fe6f2b0195d970ab6609e1063
Hal Chen <yalahao@gmail.com>
2015-04-18 08:53:34 +0200

    Pseudo code done

---
+++ ttt.rb
@@ -0,0 +1,19 @@
+# Pseudo code
+=begin
+
+Cell 1-9
+  States: x, o, empty
+
+Choose to play
+  Validate choice
+  Validate empty_cell?
+  Change cell state
+
+Change turn
+
+Check winning condition - 6 cases
+
+Display the game board
+  Update the game board
+
+=end
