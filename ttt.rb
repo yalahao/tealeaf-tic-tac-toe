4ec2c86aa475a8526178e39beca3650bdc095b83
Hal Chen <yalahao@gmail.com>
2015-04-18 09:57:44 +0200

    Added make_a_move and computer_move

--- ttt.rb
+++ ttt.rb
@@ -3,17 +3,67 @@
 
 Cell 1-9
   States: x, o, empty
+  Array [0, 1, 2, 1, 2]
+
+Start round
 
 Choose to play
   Validate choice
   Validate empty_cell?
   Change cell state
 
-Change turn
+Check game board status
+  Winning condition? - 6 cases
+  Full board?
 
-Check winning condition - 6 cases
+Computer play
+  Make a move
+  Validate move
+  Change cell state
+
+Check game board status
 
 Display the game board
   Update the game board
 
 =end
+
+require 'pry'
+
+def say(s)
+  puts "--- #{s} ---"
+end
+
+def make_a_move(cells, board)
+  say "Where to place your next move? Choose from cell 1-9. "
+  move = gets.chomp
+  if (cells.include?(move.to_i) && board[move.to_i] == 0)
+    board[move.to_i] = 1
+  else
+    say "Invalid move. Try again."
+    make_move(cells, board)
+  end
+end
+
+def computer_move(cells, board)
+  loop do
+    move = cells.sample
+    if board[move-1] == 0
+      board[move-1] = 2
+      break
+    end
+  end
+end
+
+
+
+
+
+
+board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+make_a_move(cells, board)
+computer_move(cells, board)
+
+binding.pry
