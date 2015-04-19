5062388494e5686e375a984d6fb09659d0860cec
Hal Chen <yalahao@gmail.com>
2015-04-19 04:38:07 +0200

    Changed board display style

--- tictactoe.rb
+++ tictactoe.rb
@@ -6,12 +6,12 @@
 
 def initialize_board
   board = {}
-  (1..9).each {|position| board[position] = ' '}
+  (1..9).each {|position| board[position] = '[ ]'}
   board
 end
 
 def empty_positions(board)
-  board.keys.select {|position| board[position] == ' '}
+  board.keys.select {|position| board[position] == '[ ]'}
 end
 
 def player_places_piece(board)
@@ -19,19 +19,19 @@ def player_places_piece(board)
     puts "Choose a position (from 1 to 9) to place a piece:"
     position = gets.chomp.to_i
   end until empty_positions(board).include?(position)
-  board[position] = 'X'
+  board[position] = '[X]'
 end
 
 def computer_places_piece(board)
   position = empty_positions(board).sample
-  board[position] = 'O'
+  board[position] = '[O]'
 end
 
 def check_winner(board)
   winning_lines = [[1,2,3], [4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   winning_lines.each do |line|
-    return "Player" if board.values_at(*line).count('X') == 3
-    return "Computer" if board.values_at(*line).count('O') == 3
+    return "Player" if board.values_at(*line).count('[X]') == 3
+    return "Computer" if board.values_at(*line).count('[O]') == 3
   end
   nil
 end
