73728136f8756e496c30dc9e93cd8efcca8a62f5
Hal Chen <yalahao@gmail.com>
2015-04-19 05:28:24 +0200

    Added winner and check_end_game

--- ttt_v2.rb
+++ ttt_v2.rb
@@ -12,29 +12,61 @@ def new_board
   return board
 end
 
+def display_board(board)
+  system 'clear'
+  puts
+  puts "    #{board[1]} #{board[2]} #{board[3]}"
+  puts "    #{board[4]} #{board[5]} #{board[6]}"
+  puts "    #{board[7]} #{board[8]} #{board[9]}"
+  puts
+end
+
+def empty_cells(board)
+  return board.select{|k,v| v == '[ ]'}.keys
+end
+
 def player_makes_a_move(board)
   say "Where to place your next move? Choose from cell 1-9."
   move = gets.chomp.to_i
-  if (board.keys.include?(move) && board[move] == '[ ]')
-    board[move] = '[X]'
+  if empty_cells(board).include?(move)
+    board[move] = '[O]'
   else
     say "Invalid move. Try again."
     player_makes_a_move(board)
   end
 end
 
+def computer_makes_a_move(board)
+  move = empty_cells(board).sample
+  board[move] = '[X]'
+end
+
+def winner(board)
+  winning_lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  winning_lines.each {|line|
+    if board.values_at(line).count('[O]') == 3
+      return "Player"
+    elsif board.values_at(line).count('[X]') == 3
+      return "Computer"
+    else
+      return nil
+    end
+  }
+end
+
+def check_end_game(board)
+  if winner(board)
+    say "#{winner(board)} won!"
+  elsif !empty_cells(board)
+    say "It's a tie."
+  end
+end
+
+
 
 # BEGIN original code
 
-def computer_move(cells, board)
-  loop do
-    move = cells.sample - 1
-    if board[move] == 0
-      board[move] = 2
-      break
-    end
-  end
-end
+
 
 def end_game?(board)
   if ((board[0] * board[1] * board[2] == 1) ||
@@ -73,14 +105,7 @@ def start_round(cells, board)
   start_round(cells, board)
 end
 
-def display_board(board)
-  system 'clear'
-  puts
-  puts "   #{board[1]} #{board[2]} #{board[3]}"
-  puts "   #{board[4]} #{board[5]} #{board[6]}"
-  puts "   #{board[7]} #{board[8]} #{board[9]}"
-  puts
-end
+
 def play_again?
   say "Play again? (Y/N)"
   choice = gets.chomp.downcase
@@ -106,3 +131,7 @@ say "Welcome to the Tic Tac Toe game!"
 board = new_board
 player_makes_a_move(board)
 display_board(board)
+check_end_game(board)
+computer_makes_a_move(board)
+display_board(board)
+check_end_game(board)
